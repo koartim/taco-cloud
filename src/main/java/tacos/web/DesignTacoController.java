862a8de85b78acbb1b6625e2424b94d1b236ccc3
Timothy Koar <koartimothy@gmail.com>
2020-01-30 11:08:22 -0500

    added orderController

--- src/main/java/tacos/web/DesignTacoController.java
+++ src/main/java/tacos/web/DesignTacoController.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import tacos.Ingredient;
 import tacos.Ingredient.Type;
@@ -40,8 +41,13 @@ public class DesignTacoController {
         return "design";
     }
 
-    private List<Ingredient> filterByType(
-             List<Ingredient> ingredients, Type type) {
+    @PostMapping
+    public String processDesign(Taco design) {
+        log.info("Processing design: " + design);
+        return "redirect:/orders/current";
+    }
+
+    private List<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
         return ingredients
                 .stream()
                 .filter(x -> x.getType().equals(type))
--- src/main/java/tacos/web/HomeController.java
+++ src/main/java/tacos/web/HomeController.java
@@ -8,6 +8,7 @@ public class HomeController {
 
     @GetMapping(value = "/")
     // get mapping assumes that the value returned by the annotated method is the logical name for the corresponding view
+
     public String home() {
         return "home";
     }
