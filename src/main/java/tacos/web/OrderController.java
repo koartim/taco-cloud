f1ace3864bd9d6a8dc3806e0ba655e377c1304a1
Timothy Koar <koartimothy@gmail.com>
2020-01-30 15:02:49 -0500

    bug fixes

--- src/main/java/tacos/web/OrderController.java
+++ src/main/java/tacos/web/OrderController.java
@@ -3,6 +3,7 @@ package tacos.web;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,8 +20,8 @@ public class OrderController {
     }
 
     @PostMapping
-    public String processOrder(Order order) {
+    public String processOrder(Order order, Errors errors) {
         log.info("Order submitted: " + order);
-        return "/redirect:/";
+        return "redirect:/orders/current";
     }
 }
