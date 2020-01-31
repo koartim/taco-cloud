0c4683d0def5afd2ede9ba0121da6d20f7d7837b
Timothy Koar <koartimothy@gmail.com>
2020-01-31 09:32:58 -0500

    added jdbc implementation

---
+++ src/main/java/tacos/data/JdbcIngredientRepository.java
@@ -0,0 +1,41 @@
+package tacos.data;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+import tacos.Ingredient;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@Repository
+public class JdbcIngredientRepository implements IngredientRepository{
+
+    private JdbcTemplate jdbc;
+
+    @Autowired
+    public JdbcIngredientRepository(JdbcTemplate jdbc) {
+        this.jdbc = jdbc;
+    }
+
+    public Iterable<Ingredient> findAll() {
+        return jdbc.query("select id, name, type from Ingredient", this::mapRowToIngredient);
+    }
+
+    public Ingredient findOne(String id) {
+        return jdbc.queryForObject("select id, name, type from Ingredient where id=?", this::mapRowToIngredient, id);
+    }
+
+    @Override
+    public Ingredient save(Ingredient ingredient) {
+        return null;
+    }
+
+    private Ingredient mapRowToIngredient(ResultSet rs, int RowNum) throws SQLException {
+        return new Ingredient(
+                rs.getString("id"),
+                rs.getString("name"),
+                Ingredient.Type.valueOf(rs.getString("type")));
+    }
+
+}
